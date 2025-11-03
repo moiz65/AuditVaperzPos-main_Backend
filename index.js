5ff780e820825dd9c175ec015f01041550505223
Abdul Muizz <138612881+moiz65@users.noreply.github.com>
2025-11-03 23:29:03 +0500

    Update index.js
    update db connection script

--- index.js
+++ index.js
@@ -17,11 +17,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // MySQL connection setup
-const db = mysql.createConnection({
-  database: process.env.DB_NAME,
+// const db = mysql.createConnection({
+//   database: process.env.DB_NAME,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+// });
+
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 // Checking connection to MySQL
